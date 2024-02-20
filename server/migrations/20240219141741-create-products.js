20b7070625156e6fe6bd035a0890979e1b9cf39b
Raihan162 <muhammadraihanarrafi@gmail.com>
2024-02-20 21:07:21 +0700

    feat: models and migrations, products

---
+++ server/migrations/20240219141741-create-products.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      stock: {
+        type: Sequelize.INTEGER
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      image_url: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('products');
+  }
+};
---
+++ server/models/products.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+  Model, Sequelize
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class products extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.category, {
+        foreignKey: 'category_id'
+      }),
+        this.hasMany(models.comments, {
+          foreignKey: 'products_id'
+        }),
+        this.hasMany(models.cart, {
+          foreignKey: 'products_id'
+        }),
+        this.hasMany(models.transactions, {
+          foreignKey: 'products_id'
+        }),
+        this.hasMany(models.wishlist, {
+          foreignKey: 'products_id'
+        })
+    }
+  }
+  products.init({
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    stock: DataTypes.INTEGER,
+    price: DataTypes.INTEGER,
+    image_url: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'products',
+  });
+  return products;
+};
