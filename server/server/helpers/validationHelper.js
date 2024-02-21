a701748fe252b04c075651077fbcd80ebb21722d
Raihan162 <muhammadraihanarrafi@gmail.com>
2024-02-21 11:41:50 +0700

    feat: add addProductValidation

--- server/server/helpers/validationHelper.js
+++ server/server/helpers/validationHelper.js
@@ -27,9 +27,24 @@ const loginValidation = (data) => {
     if (schema.validate(data).error) {
         throw Boom.badRequest(schema.validate(data).error);
     };
-}
+};
+
+const addProductValidation = (data) => {
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        description: Joi.string().required(),
+        price: Joi.number().required(),
+        stock: Joi.number().required(),
+        category_id: Joi.number().required()
+    });
+
+    if (schema.validate(data).error) {
+        throw Boom.badRequest(schema.validate(data.error));
+    };
+};
 
 module.exports = {
     registerValidation,
-    loginValidation
+    loginValidation,
+    addProductValidation
 };
