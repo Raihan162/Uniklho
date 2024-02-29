ecb15799e7383cb6dffd26011f6c7b29f648aea5
Muhammad Raihan Arrafi <muhammadraihanarrafi@gmail.com>
2024-02-29 22:58:47 +0700

    feat: [BE] wishlist added

--- server/server/api/wishlist.js
+++ server/server/api/wishlist.js
@@ -8,6 +8,75 @@ const Middleware = require('../middleware/authMiddleware');
 
 const fileName = 'server/api/wishlist.js';
 
+const addWishlist = async (req, res) => {
+    try {
+        const { dataToken, products_id } = req.body;
 
+        const response = await WishlistHelper.addToWishlist({ dataToken, products_id });
+
+        return res.send({
+            message: 'Add Wishlist Success',
+            response
+        });
+    } catch (error) {
+        console.log([fileName, 'Add Wishlist API', 'ERROR'], { info: `${error}` });
+        return res.send(GeneralHelper.errorResponse(error));
+    }
+};
+
+const getAllWishlist = async (req, res) => {
+    try {
+        const { dataToken } = req.body;
+
+        const response = await WishlistHelper.getAllWishlist({ dataToken });
+
+        return res.send({
+            message: 'Get All Wishlist Success',
+            response
+        });
+    } catch (error) {
+        console.log([fileName, 'Get All Wishlist API', 'ERROR'], { info: `${error}` });
+        return res.send(GeneralHelper.errorResponse(error));
+    }
+};
+
+const deleteWishlist = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { dataToken } = req.body;
+
+        const response = await WishlistHelper.deleteFromWishlist({ id, dataToken });
+
+        return res.send({
+            message: 'Delete Wishlist Success',
+            response
+        })
+    } catch (error) {
+        console.log([fileName, 'Delete Wishlist API', 'ERROR'], { info: `${error}` });
+        return res.send(GeneralHelper.errorResponse(error));
+    }
+};
+
+const getWishlistbyProduct = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const { dataToken } = req.body;
+
+        const response = await WishlistHelper.getWishlistbyProduct({ id, dataToken });
+        
+        return res.send({
+            message: 'Get Wishlist by Product Success',
+            response
+        })
+    } catch (error) {
+        console.log([fileName, 'Get Wishlist by Product API', 'ERROR'], { info: `${error}` });
+        return res.send(GeneralHelper.errorResponse(error));
+    }
+}
+
+Router.post('/add', Middleware.validateToken, addWishlist);
+Router.get('/list', Middleware.validateToken, getAllWishlist);
+Router.delete('/delete/:id', Middleware.validateToken, deleteWishlist);
+Router.get('/list/:id', Middleware.validateToken, getWishlistbyProduct);
 
 module.exports = Router;
--- server/server/helpers/wishlistHelper.js
+++ server/server/helpers/wishlistHelper.js
@@ -1,18 +1,103 @@
+const Boom = require('boom');
+const _ = require('lodash');
+require('dotenv').config();
+const moment = require('moment');
 
-const addToWishlist = () => {
+const fileName = 'server/helpers/wishlistHelpers.js';
+
+const db = require('../../models/index');
+const GeneralHelper = require('./generalHelper');
+
+const addToWishlist = async ({ dataToken, products_id }) => {
     try {
+        const checkProduct = await db.products.findOne({
+            where: {
+                id: products_id
+            }
+        });
+        if (!checkProduct){
+            return Promise.reject(Boom.badRequest('Product doesn`t exist'))
+        };
 
+        const checkWishlist = await db.wishlist.findOne({
+            where: {
+                products_id: products_id
+            }
+        });
+        if (checkWishlist){
+            await db.wishlist.destroy({
+                where: {
+                    user_id: dataToken?.id,
+                    products_id: products_id
+                }
+            })
+            return Promise.resolve(false);
+        }
+
+        await db.wishlist.create({
+            products_id,
+            user_id: dataToken?.id
+        });
+        
+        return Promise.resolve(true);
     } catch (error) {
         console.log([fileName, 'Add to Wishlist Product Helpers', 'ERROR'], { info: `${error}` });
         return Promise.reject(GeneralHelper.errorResponse(error));
     }
 };
 
-const deleteFromWishlist = async () => {
+const deleteFromWishlist = async ({ id, dataToken }) => {
     try {
-
+        await db.wishlist.destroy({
+            where: {
+                user_id: dataToken?.id,
+                products_id: id
+            }
+        })
+        return Promise.resolve(true);
     } catch (error) {
         console.log([fileName, 'Delete Product From Wishlist Helpers', 'ERROR'], { info: `${error}` });
         return Promise.reject(GeneralHelper.errorResponse(error));
     }
 };
+
+const getAllWishlist = async ({ dataToken }) => {
+    try {
+        const response = await db.wishlist.findAll({
+            where: {
+                user_id: dataToken?.id
+            },
+            include: {
+                model: db.products
+            }
+        });
+
+        return Promise.resolve(response);
+    } catch (error) {
+        console.log([fileName, 'Get All Data From Wishlist Helpers', 'ERROR'], { info: `${error}` });
+        return Promise.reject(GeneralHelper.errorResponse(error));
+    }
+};
+
+const getWishlistbyProduct = async ({ id, dataToken }) => {
+    try {
+        const response = await db.wishlist.findOne({
+            where: {
+                user_id: dataToken?.id,
+                products_id: id
+            }
+        });
+
+        return Promise.resolve(response);
+    } catch (error) {
+        console.log([fileName, 'Get Wishlit by Product Helpers', 'ERROR'], { info: `${error}` });
+        return Promise.reject(GeneralHelper.errorResponse(error));
+    }
+}
+
+module.exports = {
+    addToWishlist,
+    deleteFromWishlist,
+    getAllWishlist,
+    getWishlistbyProduct
+}
