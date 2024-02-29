369d760c6792866f7cdb2574d4e34b6680f3d295
Muhammad Raihan Arrafi <muhammadraihanarrafi@gmail.com>
2024-02-29 23:01:43 +0700

    feat: [FE] add domain api

--- client/src/domain/api.js
+++ client/src/domain/api.js
@@ -17,6 +17,17 @@ const urls = {
 
   login: 'login',
   register: 'register',
+
+  rajaongkirProvince: 'rajaongkir/province',
+  rajaongkirCity: 'rajaongkir/city',
+  rajaongkirCost: 'rajaongkir/cost',
+
+  createTransaction: 'transaction/create',
+  updatePayment: 'transaction/payment/update',
+  transactionList: 'transaction/list',
+
+  addWishlist: 'wishlist/add',
+  getWishlist: 'wishlist/list',
 };
 
 export const callAPI = async (endpoint, method, header = {}, params = {}, data = {}) => {
@@ -42,20 +53,25 @@ export const callAPI = async (endpoint, method, header = {}, params = {}, data =
 export const ping = () => callAPI(urls.ping, 'get');
 
 export const getProduct = () => callAPI(urls.productList, 'GET');
-
-export const getCategory = () => callAPI(urls.categoryList, 'GET');
-
+export const getProductDetailApi = (id) => callAPI(`${urls.productList}/${id}`, 'GET');
 export const addProductDomain = (formData) => callAPI(urls.addProduct, 'POST', {}, {}, formData);
 
+export const getCategory = () => callAPI(urls.categoryList, 'GET');
 export const loginApi = (data) => callAPI(urls.login, 'POST', {}, {}, data);
-
 export const registerApi = (data) => callAPI(urls.register, 'POST', {}, {}, data);
 
-export const getProductDetailApi = (id) => callAPI(`${urls.productList}/${id}`, 'GET');
-
 export const addToCart = (data) => callAPI(urls.addToCart, 'POST', {}, {}, data);
-
 export const getDataCartApi = () => callAPI(urls.cartList, 'GET');
-
 export const deleteCartApi = (id) => callAPI(`${urls.cartDelete}/${id}`, 'DELETE');
 
+export const provinceAPI = () => callAPI(urls.rajaongkirProvince, 'GET');
+export const cityAPI = (id) => callAPI(`${urls.rajaongkirCity}?province_id=${id}`,);
+export const serviceAPI = ( destination, courier ) => callAPI(`${urls.rajaongkirCost}?destination=${destination}&courier=${courier}`, 'POST');
+
+export const createTransactionAPI = (data) => callAPI(urls.createTransaction, 'POST', {}, {}, data);
+export const updatePaymentAPI = (data) => callAPI(urls.updatePayment, 'PATCH', {}, {}, data);
+export const getTransactionAPI = (data) => callAPI(urls.transactionList, 'GET', {}, {}, data);
+
+export const addWishlistAPI = (data) => callAPI(urls.addWishlist, 'POST', {}, {}, data);
+export const getWishlistAPI = () => callAPI(urls.getWishlist, 'GET');
+export const getWishlistByProductAPI = (id) => callAPI(`${urls.getWishlist}/${id}`, 'GET');
