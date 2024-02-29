2e3f22484b9bef2d15e0942fa8321e12f89d885e
Muhammad Raihan Arrafi <muhammadraihanarrafi@gmail.com>
2024-02-29 23:01:25 +0700

    feat: [FE] add menu

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -10,18 +10,24 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import NightsStayIcon from '@mui/icons-material/NightsStay';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 import { setLoading, setLocale, setTheme } from '@containers/App/actions';
 
 import Logo from '../../../assets/cart.png'
 
 import classes from './style.module.scss';
-import { Badge, Button } from '@mui/material';
+import { Badge, Button, Divider, IconButton, ListItemIcon } from '@mui/material';
 import { jwtDecode } from 'jwt-decode';
 import { setLogin, setToken } from '@containers/Client/actions';
 import { getDataCart } from '@pages/Cart/action';
+import { Logout, PersonAdd, Settings } from '@mui/icons-material';
 
 const Navbar = ({ title, locale, theme, login, token, cart }) => {
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const openProfile = Boolean(anchorEl);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
@@ -68,6 +74,22 @@ const Navbar = ({ title, locale, theme, login, token, cart }) => {
     navigate('/register');
   };
 
+  const handleClickProfile = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseProfile = () => {
+    setAnchorEl(null);
+  };
+
+  const handleMyOrder = () => {
+    navigate('/my-order')
+  };
+
+  const handleWishlist = () => {
+    navigate('/wishlist')
+  }
+
   const handleToLogout = () => {
     dispatch(setLoading(true));
     setTimeout(() => {
@@ -79,7 +101,9 @@ const Navbar = ({ title, locale, theme, login, token, cart }) => {
   };
 
   useEffect(() => {
-    dispatch(getDataCart())
+    if(token){
+      dispatch(getDataCart())
+    }
   }, [])
 
   return (
@@ -93,24 +117,93 @@ const Navbar = ({ title, locale, theme, login, token, cart }) => {
           {/* <div className={classes.theme} onClick={handleTheme} data-testid="toggleTheme">
             {theme === 'light' ? <NightsStayIcon /> : <LightModeIcon />}
           </div> */}
-          <div onClick={goToCart} className={classes.buttonIcon}>
-            <Badge badgeContent={cart?.length} color='error'>
-              <ShoppingCartIcon />
-            </Badge>
-          </div>
+          {
+            token?
+              <div onClick={goToCart} className={classes.buttonIcon}>
+                  <Badge badgeContent={cart?.length} color='error'>
+                    <ShoppingCartIcon />
+                  </Badge>
+                </div>
+                :
+              null
+          }
           <div className={classes.toggle} onClick={handleClick}>
             <Avatar className={classes.avatar} src={locale === 'id' ? '/id.png' : '/en.png'} />
             <div className={classes.lang}>{locale}</div>
             <ExpandMoreIcon />
           </div>
           {
-            location.pathname === '/login' || location?.pathname === '/register' ?
+            location?.pathname === '/login' || location?.pathname === '/register' ?
               null
               :
               login ?
-                <button onClick={handleToLogout} className={classes.buttonAvatar}>
-                  <Avatar alt={decodeToken?.name} src={`${decodeToken?.photo_profile ? decodeToken?.photo_profile : "/static/images/avatar/1.jpg"}`} />
-                </button>
+                // <button onClick={handleToLogout} className={classes.buttonAvatar}>
+                //   <Avatar alt={decodeToken?.name} src={`${decodeToken?.photo_profile ? decodeToken?.photo_profile : "/static/images/avatar/1.jpg"}`} />
+                // </button>
+                <>
+                  <IconButton
+                    onClick={handleClickProfile}
+                    size="small"
+                    sx={{ ml: 2 }}
+                    aria-controls={openProfile ? 'account-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={openProfile ? 'true' : undefined}
+                  >
+                    <Avatar alt={decodeToken?.name} src={`${decodeToken?.photo_profile ? decodeToken?.photo_profile : "/static/images/avatar/1.jpg"}`} />
+                  </IconButton>
+                    <Menu
+                    anchorEl={anchorEl}
+                    id="account-menu"
+                    open={openProfile}
+                    onClose={handleCloseProfile}
+                    onClick={handleCloseProfile}
+                    PaperProps={{
+                      elevation: 0,
+                      sx: {
+                        overflow: 'visible',
+                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                        mt: 1.5,
+                        '& .MuiAvatar-root': {
+                          width: 32,
+                          height: 32,
+                          ml: -0.5,
+                          mr: 1,
+                        },
+                        '&::before': {
+                          content: '""',
+                          display: 'block',
+                          position: 'absolute',
+                          top: 0,
+                          right: 20,
+                          width: 10,
+                          height: 10,
+                          bgcolor: 'background.paper',
+                          transform: 'translateY(-50%) rotate(45deg)',
+                          zIndex: 0,
+                        },
+                      },
+                    }}
+                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                  >
+                    <MenuItem onClick={handleClose}>
+                      <Avatar /> Profile
+                    </MenuItem>
+                    <MenuItem onClick={handleMyOrder}>
+                      <ReceiptIcon color='disabled' /> Transaction
+                    </MenuItem>
+                    <MenuItem onClick={handleWishlist}>
+                      <FavoriteIcon color='disabled' /> Wishlist
+                    </MenuItem>
+                    <Divider />
+                    <MenuItem onClick={handleToLogout}>
+                      <ListItemIcon>
+                        <Logout fontSize="small" />
+                      </ListItemIcon>
+                      Logout
+                    </MenuItem>
+                  </Menu>
+                </>
                 :
                 <>
                   <Button onClick={goToLogin}>
